9315ea568b083760c75b816d3bc24f3c5b720ff6
AntoineBrosseau <antoine.brosseau@edu.ece.fr>
2025-11-26 11:24:44 +0100

    stat

--- src/composants/Stat.jsx
+++ src/composants/Stat.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Stat.css';
-import IconCartable from '../assets/images/cartable.svg';
-import IconRegle from '../assets/images/regle.svg';
-import IconExport from '../assets/images/export.svg';
+import Cartable from '../assets/images/cartable.svg';
+import Regle from '../assets/images/regle.svg';
+import Bibliotheque from '../assets/images/bibliothèque.svg';
 
 function StatCard({ title, value, sub, icon, iconClass }) {
   return (
@@ -25,9 +25,9 @@ export default function Stat() {
       <h3 className="stat-section-title">MES STATISTIQUES</h3>
 
       <div className="stat-grid">
-        <StatCard title="Projets actifs" value="2" sub="+1 ce mois-ci" icon={<img src={IconCartable} alt="cartable" />} iconClass="icon-blue" />
-        <StatCard title="m² mesurés ce mois" value="1254" sub="+326 vs mois dernier" icon={<img src={IconRegle} alt="regle" />} iconClass="icon-green" />
-        <StatCard title="Exports récents" value="8" sub="Dernier: 2 jours" icon={<img src={IconExport} alt="export" />} iconClass="icon-blue" />
+        <StatCard title="Projets actifs" value="2" sub="↑ +1 ce mois-ci" icon={<img src={Cartable} alt="Projets actifs" />} iconClass="icon-blue" />
+        <StatCard title="m² mesurés ce mois" value="1254" sub="↑ +326 vs mois dernier" icon={<img src={Regle} alt="m² mesurés" />} iconClass="icon-green" />
+        <StatCard title="Nombre de bibliothèques" value="5" sub="↑ +2 ce mois-ci" icon={<img src={Bibliotheque} alt="Bibliothèques" />} iconClass="icon-red" />
       </div>
     </section>
   );
